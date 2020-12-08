0defbac2efb159b28cbad7a08f635a06586df78d
sarom-torres <saromtorres@gmail.com>
2020-12-07 23:55:58 -0300

    pequenas alterações

---
+++ AULAS-2020.2/aula_COM1_20_11_2020.m
@@ -0,0 +1,25 @@
+clear all;
+close all;
+clc
+A = 1;
+fm = 1e3;
+fc = 10e3;
+N = 100;
+fs = N*fc;
+ts = 1/fs;
+t_final = 1;
+filtro_pb = 
+t = [0:ts:t_final];
+y = cos(2*pi*100*t) + cos(2*pi*500*t);
+T = length(y);
+f = [-fs/2:1:fs/2];
+Y = fft(y);
+Y = fftshift(Y);
+figure(1)
+subplot(211)
+plot(t,y)
+xlim([0 20e-3])
+subplot(212)
+plot(f,abs(Y)/length(y))
+xlim([-15e3 15e3])
+
